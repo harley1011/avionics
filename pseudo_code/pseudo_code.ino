a586b4ea8042d008c5b1e4949362061a7e9b5c2d
A-Hubbell <austin.hubbell@gmail.com>
2014-01-29 14:45:27 -0500

    Control System Pseudo Code
    General layout of rocket control system and conditions for changing
    from one mode to another. Work in progress

---
+++ pseudo_code/pseudo_code.ino
@@ -0,0 +1,53 @@
+const int LED_PIN = 0;
+const int MODE1_MODE2 = 500; //500 ft altitude detected, switch from mode 1 to mode 2
+const int DROGUE_EMATCH = 1; //Pin connected to drogue parachute e-match
+const int MAIN_EMATCH = 2; //Pin connected to main parachute e-match
+const int ACCEL_THRESH = 3; // Acceleration upper threshold for apogee detection
+
+void setup()
+{
+  Serial.begin(9600)
+}
+
+void loop()
+{
+ 
+ delay(500) // intial delay for all electronics
+ 
+ //Read in pressure values
+ //Average pressure calculations (to calculate mainChutePressure)
+ 
+  do
+  {
+  //Read 9-DOF data
+  //Read BMP180 data
+  //Read GPS data
+  //Write data to Xbee
+  //Write data to serial logger
+  }
+  
+  while(rocketAltitude<mode1_mode2) 
+ 
+ if (vertAccel < ACCEL_THRESH) 
+ {
+  
+  digitalWrite(DROGUE_EMATCH, HIGH);
+  //Read 9-DOF data
+  //Read BMP180 data
+  //Read GPS data
+  //Write data to Xbee
+  //Write data to serial logger
+  //Include extra statement in serial transmission to indicated when parachute was deployed
+  
+ }
+  
+ else
+ {
+  //Read 9-DOF data
+  //Read BMP180 data
+  //Read GPS data
+  //Write data to Xbee
+  //Write data to serial logger
+ }
+
+}
