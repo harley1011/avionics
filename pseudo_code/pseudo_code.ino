94015d81fcdcdad68ef6cb4834b5dd4dc58087d5
A-Hubbell <A-Hubbell@users.noreply.github.com>
2014-01-29 14:55:46 -0500

    Update pseudo_code.ino

--- pseudo_code/pseudo_code.ino
+++ pseudo_code/pseudo_code.ino
@@ -1,12 +1,17 @@
+//Global Variables and Constants
+
 const int LED_PIN = 0;
 const int MODE1_MODE2 = 500; //500 ft altitude detected, switch from mode 1 to mode 2
-const int DROGUE_EMATCH = 1; //Pin connected to drogue parachute e-match
-const int MAIN_EMATCH = 2; //Pin connected to main parachute e-match
+const int DROGUE_EMATCH_PIN = 1; //Pin connected to drogue parachute e-match
+const int MAIN_EMATCH_PIN = 2; //Pin connected to main parachute e-match
 const int ACCEL_THRESH = 3; // Acceleration upper threshold for apogee detection
 
 void setup()
 {
-  Serial.begin(9600)
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(DROGUE_EMATCH_PIN, OUTPUT);
+  pinMode(MAIN_EMATCH_PIN, OUTPUT);
 }
 
 void loop()
@@ -19,35 +24,35 @@ void loop()
  
   do
   {
-  //Read 9-DOF data
-  //Read BMP180 data
-  //Read GPS data
-  //Write data to Xbee
-  //Write data to serial logger
+    //Read 9-DOF data
+    //Read BMP180 data
+    //Read GPS data
+    //Write data to Xbee
+    //Write data to serial logger
   }
   
   while(rocketAltitude<mode1_mode2) 
  
- if (vertAccel < ACCEL_THRESH) 
- {
+  if (vertAccel < ACCEL_THRESH) 
+  {
   
-  digitalWrite(DROGUE_EMATCH, HIGH);
-  //Read 9-DOF data
-  //Read BMP180 data
-  //Read GPS data
-  //Write data to Xbee
-  //Write data to serial logger
-  //Include extra statement in serial transmission to indicated when parachute was deployed
+    digitalWrite(DROGUE_EMATCH_PIN, HIGH);
+    //Read 9-DOF data
+    //Read BMP180 data
+    //Read GPS data
+    //Write data to Xbee
+    //Write data to serial logger
+    //Include extra statement in serial transmission to indicated when parachute was deployed
   
- }
+  }
   
- else
- {
-  //Read 9-DOF data
-  //Read BMP180 data
-  //Read GPS data
-  //Write data to Xbee
-  //Write data to serial logger
- }
+  else
+  {
+    //Read 9-DOF data
+    //Read BMP180 data
+    //Read GPS data
+    //Write data to Xbee
+    //Write data to serial logger
+  }
 
 }
